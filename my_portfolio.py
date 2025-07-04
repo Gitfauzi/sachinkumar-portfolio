6ac7fc2f47f220c46352bedefea72d5c3e103035
Sachin Kumar <125558937+Gitfauzi@users.noreply.github.com>
2025-07-04 13:48:23 +0530

    commited

--- my_portfolio.py
+++ my_portfolio.py
@@ -43,5 +43,6 @@ def contact():
 
     return render_template('contact.html')
 
-if __name__ =="__main__":
-    app.run(debug = True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
+
