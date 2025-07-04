816376418473fc60bce70c3d0bb8e676bbb81a71
Sachin Kumar <125558937+Gitfauzi@users.noreply.github.com>
2025-07-04 13:05:44 +0530

    First commit website

---
+++ my_portfolio.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.secret_key = "a_really_secret_key_mxcode_123" 
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///user.db"
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    sno  = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(200), nullable = False)
+    message = db.Column(db.String(255), nullable = False)
+    datetime = db.Column(db.DateTime, default = datetime.utcnow)
+
+with app.app_context(): # here a user.db file will be made which i can see in sqlite viewer online
+    db.create_all()
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+
+@app.route("/contact", methods = ["GET", "POST"])
+def contact():
+    if request.method == 'POST':
+        email = request.form['email']
+        message = request.form['message']
+
+        # Create new user object
+        new_entry = User(email=email, message=message)
+
+        # Save to DB
+        db.session.add(new_entry)
+        db.session.commit()
+        flash("Thank you for contacting me. I'll get back to you soon!", "success")
+
+    return render_template('contact.html')
+
+if __name__ =="__main__":
+    app.run(debug = True)
---
+++ readme.md
@@ -0,0 +1,24 @@
+Sachin Kumar – Portfolio Website
+
+This is my personal portfolio website built using Flask, Bootstrap, and SQLite. It showcases my projects, skills, contact form, and resume. The site is fully responsive and deployed online for easy access by recruiters and collaborators.
+
+Tech Stack
+Frontend: HTML, CSS, Bootstrap 5
+Backend: Python (Flask)
+Database: SQLite (with SQLAlchemy)
+Deployment: Render.com
+Version Control: Git + GitHub
+
+Features:
+
+- Dark-mode modern UI with tech-themed background
+
+- Project cards with GitHub and Medium links
+
+- Contact form that stores messages in SQLite database
+
+- Resume download button
+
+
+Contact
+If you want to get in touch, use the contact form on the website or email me at: techfebr23@gmail.com
